1f6fa3744792d53c59fc2b8a1ade16b80b75ce4a
Michelk55 <123950145+Michelk55@users.noreply.github.com>
2023-10-20 09:07:46 -0300

    ponderada 1

---
+++ SEMANA_01/01_CODIGOS/ponderada1.ino
@@ -0,0 +1,20 @@
+int counter;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  for (counter = 0; counter < 10; ++counter) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+}
